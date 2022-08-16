e93e41b023c50c824f5828a5c091d17279c8f00a
HCrescent <HCrescentMoon@gmail.com>
2022-08-15 22:33:58 -0500

    Made my solution more efficient with outside help and learning, extra verbose comments to make sure I can understand the concept that was given to me when I return to reuse the function in future problems.

--- Python/001-050/Problem 7.py
+++ Python/001-050/Problem 7.py
@@ -1,45 +1,38 @@
 """Project Euler Problem 7 - 10001st prime"""
-import time
-start = time.time()
 
 
-def factorize(value):
-	"""Determines if given value is prime or not
+def isPrime(value):
+	""" Determines if a number is prime, for efficiency sake we are only testing values greater than 2
+	to avoid edge cases 1 being not prime and 2 being prime
 
-	:param value: int - number we are going to factorize
-	:return: Bool - Truthiness of prime status
+	:param value: Int - Number to check if prime
+	:return: Bool - Our truthiness value of prime status
 	"""
-
-	searching = True
-	prime = False
-	# run until our first prime factor is found
-	while searching:
-		# for loop until you hit the first possible divisor, guaranteed to be prime
-		# starts at 3 because 1 is not prime and we are skipping 2 as the only even prime
-		# ends at value (if largest factor is itself), only check odd numbers to save time
-		for number in range(3, value + 1, 2):
-			# if number divides evenly check if our first prime is itself of not
-			if value % number == 0:
-				if number < value:
-					searching = False
-					break
-				prime = True
-				searching = False
-	return prime
+	# our upper range bound is the square root rounded plus 1 to offset the exclusive end of range function
+	# the reason for the square root is a shortcut, a whole number factor will never be more than the square root.
+	# then for extra efficiency we increment by two to avoid checking even value which will never be a factor of odd#
+	for each in range(3, int(value ** .5)+1, 2):
+		# if value mod each is zero we found a factor too early and we know its not prime
+		if value % each == 0:
+			return False
+	# reaching this line means we never found a factor and its Prime
+	return True
 
 
 def countPrimes(number):
+	""" Finds the enumerated prime number excluding edge case 2 which is accounted for in the calculation
+
+	:param number: Int - the enumeration of the prime we wish to find
+	:return: - Int - the desired calculated prime number called for by the function
+	"""
 	accumulator = 1
 	primesFound = 1  # start at 1 because we will never calculate the first prime of 2
 	while primesFound < number:
 		accumulator += 2  # skip even numbers
-		if factorize(accumulator):
+		if isPrime(accumulator):
 			primesFound += 1
 	return accumulator
 
 
 if __name__ == "__main__":
-	print(countPrimes(10001))
-	end = time.time()
-	total_time = end - start
-	print("\n" + str(total_time))
+	print("The 10,001st Prime number is: ", countPrimes(10001))
