e973b250f39cb7086de14637ec16f6aedf00145e
HCrescent <HCrescentMoon@gmail.com>
2022-08-26 14:06:30 -0500

    Problem 3, prime factorization, made more efficient but calculations for arbitrarily large factors remains a problem

--- Python/001-050/Problem 3.py
+++ Python/001-050/Problem 3.py
@@ -1,20 +1,24 @@
 """Project Euler Problem 3 - Largest prime factor"""
 
 
-def factors(value):
+def primeFactorize(value):
 	"""Returns a list of prime factors of given whole number.
 
 	:param value: int - number we are going to factorize
 	:return: list - list of prime factors
 	"""
-
+	# always check 2 first
+	number = 2
 	prime_factors = []
 	# run until our value is reduced to 1
 	while value > 1:
-		# for loop until you hit the first possible divisor, guaranteed to be prime
-		# starts at 2 because 1 is not prime
-		# ends at value (if largest factor is itself), +1 because range() end exclusive bound
-		for number in range(3, value + 1, 2):
+		# always start from the largest prime factor found last
+		if len(prime_factors) > 0:
+			number = prime_factors[-1]
+		# while loop until you hit the first possible divisor, guaranteed to be prime
+		# starts at 2 because 1 is not prime and always divisible
+		# ends at value/2 (if largest factor is itself)
+		while number < value/2:
 			# if number divides evenly
 			if value % number == 0:
 				# use floored division to prevent expanding int to float
@@ -23,8 +27,22 @@ def factors(value):
 				prime_factors.append(number)
 				# breaks after first prime factor
 				break
+			# this check is so we can include prime number 2 in our factor checks
+			# if LSB of number is 1 we are odd and can skip all the rest of even numbers
+			# else move from 2 -> 3
+			if number & 1:
+				number += 2
+			else:
+				number += 1
+		# since we stopped early to save checking obviously wrong factors
+		if number > value/2:
+			# at this point there are no more factors than our leftover value itself
+			prime_factors.append(value)
+			# lowering value to 1 to prepare to exit function
+			value //= value
+
 	return prime_factors
 
 
 if __name__ == "__main__":
-	print("Prime factors of given value: ", factors(600851475143))
+	print("Prime factors of given value: ", primeFactorize(600851475143))
