6ef21a0fc1d852203a887ccfe31ab57711d8c256
HCrescent <HCrescentMoon@gmail.com>
2022-09-04 17:42:37 -0500

    Problem 20 solution, trivial in python, also factorial function built in to math library

---
+++ Python/001-050/Problem 020.py
@@ -0,0 +1,17 @@
+"""Project Euler Problem 20 - Factorial digit sum"""
+
+
+def Factorial(number):
+	""" simple factorial function, can also use import math for math.factorial() for a faster built in function
+
+	:param number: Int - the number we want to calculate the factorial of
+	:return: Int - the final product
+	"""
+	product = 1
+	for each in range(1, number+1):
+		product *= each
+	return product
+
+
+if __name__ == "__main__":
+	print("The sum of the digits in the number 100! is", sum(list(map(int, str(Factorial(100))))))
