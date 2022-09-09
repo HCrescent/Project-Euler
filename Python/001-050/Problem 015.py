860ba029c7932fb3521f2c412d15b8453b94b4b4
HCrescent <HCrescentMoon@gmail.com>
2022-09-09 16:38:18 -0500

    Problem 15 solution based on a simple formula for combinatorics, I want more practice with permutations and combinatorics to really get a grasp on them

--- Python/001-050/Problem 015.py
+++ Python/001-050/Problem 015.py
@@ -1,21 +1,33 @@
 """Project Euler Problem 15 - Lattice paths"""
-import time
-start = time.time()
 
 
-def recursivePath(bound, x=0, y=0):
-	paths = 0
-	if x < bound:
-		paths += recursivePath(bound, x+1, y)
-	if y < bound:
-		paths += recursivePath(bound, x, y+1)
-	if x == bound and y == bound:
-		paths += 1
-	return paths
+def factorial(number):
+	""" simple factorial function, can also use import math for math.factorial() for a faster built-in function
+
+	:param number: Int - the number we want to calculate the factorial of
+	:return: Int - the final product
+	"""
+	product = 1
+	for each in range(1, number+1):
+		product *= each
+	return product
+
+
+def combinatorics(n, k):
+	""" Simple combinatorics formula for n choose k, n! / k!(n-k)!
+	This approach works for the problem because any route from one corner of a grid to the opposite corner is going
+	to be for grid size k, k right moves, and k left moves, in every combination of possibilities to slot into
+	n = 2k moves total, every possible way you can slot 20 identical moves into 40 total (the other 20 moves go in
+	the leftover vacant slots)
+
+	:param n: Int - Number to choose from
+	:param k: Int - total of choices
+	:return: Int - the solution to formula n! / k!(n-k)!
+	"""
+	numerator = factorial(n)
+	denominator = factorial(k) * factorial(n - k)
+	return numerator // denominator
 
 
 if __name__ == "__main__":
-	print(recursivePath(11))
-	end = time.time()
-	total_time = end - start
-	print("\n" + str(total_time))
+	print("The number of routes with only right and down turns of a 20 by 20 grid:", combinatorics(40, 20))
