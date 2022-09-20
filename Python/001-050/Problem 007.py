372cbe84c30569aabb994327e621799c086d940e
HCrescent <HCrescentMoon@gmail.com>
2022-09-20 18:47:09 -0500

    fix isPrime function catching and treating negative values appropriately

--- Python/001-050/Problem 007.py
+++ Python/001-050/Problem 007.py
@@ -2,14 +2,14 @@
 
 
 def isPrime(value):
-	""" Determines if a number is prime, for efficiency sake we are only testing values greater than 2
+	""" Determines if a number is prime, for efficiency’s sake we are only testing values greater than 2
 	to avoid edge cases 1 being not prime and 2 being prime
 
 	:param value: Int - Number to check if prime
 	:return: Bool - Our truthiness value of prime status
 	"""
-	# catch even values if driving code doesnt skip 1 or even numbers
-	if value % 2 == 0 or value == 1:
+	# catch even values if driving code doesn't skip 1 or even numbers, and catch negative numbers
+	if value % 2 == 0 or value < 2:
 		if value == 2:
 			return True
 		return False
@@ -17,7 +17,7 @@ def isPrime(value):
 	# the reason for the square root is a shortcut, a whole number factor will never be more than the square root.
 	# then for extra efficiency we increment by two to avoid checking even value which will never be a factor of odd#
 	for each in range(3, int(value ** .5)+1, 2):
-		# if value mod each is zero we found a factor too early and we know its not prime
+		# if value mod each is zero we found a factor too early, and we know it's not prime
 		if value % each == 0:
 			return False
 	# reaching this line means we never found a factor and its Prime
