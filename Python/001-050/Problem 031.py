ff893c2a5455a7d2e2aef4452a8830fffde94980
HCrescent <HCrescentMoon@gmail.com>
2022-09-28 19:28:00 -0500

    31 WIP

--- Python/001-050/Problem 031.py
+++ Python/001-050/Problem 031.py
@@ -1,29 +1,37 @@
 """Project Euler Problem 31 - Coin sums"""
 
 
-def unique_denomination_sums(denominations, total, count=0):
+def unique_denomination_sums(denominations, total, current_smallest, count=0):
 	""" counts how many ways you can make change for small totals of cash (given in smallest units)
 
-	:param denominations: list - the available coin denominations, pass in descending order
-	:param total: int - the total number we want to find unique partitions of
-	:param count: int -
-	:return: int -
+	:param denominations:
+	:param total:
+	:param current_smallest:
+	:param count:
+	:return:
 	"""
 	# terminating section
-	if total == 0:
-		count += 1
+	# if total is less the the second smallest denomination
+	if total < denominations[1]:
+		if total == 0:
+			count += 1
+		elif total % denominations[0] == 0:
+			count += 1
 		return count
 	# main section
-	while total > 0:
-		for each in denominations:
-			if total >= each:
+	for each in denominations[::-1]:
+		if total >= each and each <= current_smallest:
+			if each != 1:
 				new_total = total - each
-				count = unique_denomination_sums(denominations, new_total, count)
+			else:
+				new_total = 0
+			current_smallest = each
+			count = unique_denomination_sums(denominations, new_total, current_smallest, count)
 	return count
 
 
 if __name__ == "__main__":
-	coins = [1, 5, 10, 25, 50]
+	coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-	print(unique_denomination_sums(coins[::-1], 50))
+	print(unique_denomination_sums(coins, 200, coins[-1]))
 
