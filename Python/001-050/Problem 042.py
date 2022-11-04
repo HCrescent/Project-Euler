3806e93e25765c3cd3ac349579f868e71b8c920f
HCrescent <HCrescentMoon@gmail.com>
2022-11-04 15:31:49 -0500

    Problem 42 finished solution. Simple solution.

---
+++ Python/001-050/Problem 042.py
@@ -0,0 +1,40 @@
+"""Project Euler Problem 42 - Coded triangle numbers"""
+with open("text inputs/Problem 042.txt", 'r') as infile:
+	# formatting the data with a single line list comprehension
+	words = [_.strip("\"") for _ in infile.read().split(",")]
+keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letter_scores = {each: i+1 for (i, each) in enumerate(keys)}
+
+
+def triangleNumber(n):
+	""" returns nth triangle number using formula n(n+1)/2
+
+	:param n: Int - number 'n'
+	:return: Int - triangle number
+	"""
+	return int(n*(n+1)/2)
+
+
+def scoreWords(words_list):
+	""" Creates a list of scored names with the scoring rules dictated by the problem question:
+	each word is scored by the sum of integer conversions of its letters
+	:param words_list: List - list of words
+	:return: List - list of word scores
+	"""
+	score_list = []
+	for word in words_list:
+		score = 0
+		for letter in word:
+			score += letter_scores[letter]
+		score_list.append(score)
+	return score_list
+
+
+if __name__ == "__main__":
+	triangle_numbers = {triangleNumber(_) for _ in range(1, 1000)}
+	scores_list = scoreWords(words)
+	triangle_count = 0
+	for each in scores_list:
+		if each in triangle_numbers:
+			triangle_count += 1
+	print("The number of words in the text file that translate into triangle numbers:", triangle_count)
